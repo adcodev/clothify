6cfccc934db20577b34a139011950d5390f6408f
aldair <casafrancadev@gmail.com>
2026-04-29 14:34:38 -0500

    add routing configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+@Component ({
+    selector: "app-root",
+    templateUrl: "app.html",
+    styleUrl: "app.css"
+
 })
-export class App {
-  protected readonly title = signal('clothify');
+export class App{
+
 }
