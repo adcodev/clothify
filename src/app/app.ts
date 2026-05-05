bdc10e72c3cbb81eb32507c074d4743690222bc0
aldair <casafrancadev@gmail.com>
2026-05-05 10:41:11 -0500

    Actualiza estilos y header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { Header } from "./shared/components/header/header";
+import { Home } from "./features/home/home";
 
 
 
 @Component ({
     selector: 'app-root',
     templateUrl: 'app.html',
-    imports: [Header,RouterOutlet]
+    imports: [Header,Home,RouterOutlet]
 })
 
 export class App {
